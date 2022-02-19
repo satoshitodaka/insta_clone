b12a603061be52425adedadd9e2c33b7b7635978
satoshitodaka <satoshitodaka0705@gmail.com>
2022-02-19 14:11:10 +0900

    [Fix]その他の修正
    未ログイン時を考慮
    JSとCSSの読み込み漏れ対応

--- app/assets/javascripts/mypage.js
+++ app/assets/javascripts/mypage.js
@@ -1,3 +1,8 @@
+//= require jquery3
+//= require popper
+//= require rails-ujs
+//= require bootstrap-material-design/dist/js/bootstrap-material-design.js
+
 function previewFileWithId(selector) {
   const target = this.event.target;
   const file = target.files[0];
