4c5b8db960e85ba533b6aa01119df9321d5c8af4
satoshitodaka <satoshitodaka0705@gmail.com>
2022-02-23 11:37:54 +0900

    [Add]Activityの機能実装

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -20,7 +20,10 @@
 #
 #  fk_rails_...  (user_id => users.id)
 #
+
 class Activity < ApplicationRecord
-  belongs_to :subject
+  belongs_to :subject, polymorphic: true
   belongs_to :user
+
+  enum action_type: { commented_to_own_post: 0, liked_to_own_post: 1, followed_me: 2 }
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -19,9 +19,18 @@
 #  fk_rails_...  (post_id => posts.id)
 #  fk_rails_...  (user_id => users.id)
 #
+
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
   validates :body, presence: true, length: { maximum: 1000 }
+
+  after_create_commit :create_activities
+
+  private
+
+  def create_activities
+    Activity.create(subject: self, user: post.user, action_type: :commented_to_own_post)
+  end
 end
