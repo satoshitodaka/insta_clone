b236825e627a7425ad8863292b7f649e498cf9ea
satoshitodaka <satoshitodaka0705@gmail.com>
2022-02-28 23:57:23 +0900

    [Fix]軽微な修正
    readアクションの内容の検証のため、コメントアウトしていた箇所の戻し忘れの修正

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -4,6 +4,6 @@ class ActivitiesController < ApplicationController
   def read
     activity = current_user.activities.find(params[:id])
     activity.read! if activity.unread?
-    # redirect_to activity.redirect_path
+    redirect_to activity.redirect_path
   end
 end
