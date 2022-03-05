4b33fe08fd47f6cee43d860f795d413ff79761a4
satoshitodaka <satoshitodaka0705@gmail.com>
2022-03-06 08:47:11 +0900

    [Add]sinatraの導入とダッシュボードの追加

--- Gemfile
+++ Gemfile
@@ -39,6 +39,7 @@ gem 'carrierwave', '~> 2.0'
 gem 'kaminari'
 gem 'config'
 gem 'sidekiq'
+gem 'sinatra'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,10 @@
 #      rails_direct_uploads POST   /rails/active_storage/direct_uploads(.:format)                                           active_storage/direct_uploads#create
 
 Rails.application.routes.draw do
-  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+    mount Sidekiq::Web, at: '/sidekiq'
+  end
   root to: 'posts#index'
 
   get '/login', to: 'user_sessions#new'
