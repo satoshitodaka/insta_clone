20d8b6cbcd08c5e7810a0bfaf9cfb4eaf4814bc5
satoshitodaka <satoshitodaka0705@gmail.com>
2022-03-09 07:06:47 +0900

    [Fix]routes.rbに不足を追記
    require 'sidekiq/web'が洩れていたので追記

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,8 @@
 # update_rails_disk_service PUT    /rails/active_storage/disk/:encoded_token(.:format)                                      active_storage/disk#update
 #      rails_direct_uploads POST   /rails/active_storage/direct_uploads(.:format)                                           active_storage/direct_uploads#create
 
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
