4b7322b3698c26fffad17a3305b0736f5fc0b086
satoshitodaka <satoshitodaka0705@gmail.com>
2022-04-07 19:20:26 +0900

    [Fix]ログインのスペックが通らなかったためログインフォームを修正

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,7 +14,7 @@
 #
 # See https://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
-  config.before(;each, type: :system) do
+  config.before(:each, type: :system) do
     driven_by :selenium, using: :headless_chrome, screen_size: [1920, 1080]
   end
   # rspec-expectations config goes here. You can use an alternate
